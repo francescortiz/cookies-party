0632f0c90b8c9ca2c8ea9a2fd3d6ed50fc394822
Francesc Ortiz <francescortiz@gmail.com>
2016-02-23 16:59:09 +0100

    initial commit

---
+++ jquery.cookies-party.js
@@ -0,0 +1,47 @@
+if (!jQuery.cookiesParty) {
+    (function ($) {
+
+        $.cookiesParty = {
+            message: ''
+        };
+
+        function createCookie(name, value, days) {
+            var expires = "";
+            if (days) {
+                var date = new Date();
+                date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+                expires = "; expires=" + date.toUTCString();
+            }
+            document.cookie = name + "=" + value + expires + "; path=/";
+        }
+
+        function readCookie(name) {
+            var nameEQ = name + "=";
+            var ca = document.cookie.split(';');
+            for (var i = 0; i < ca.length; i++) {
+                var c = ca[i];
+                while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+                if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+            }
+            return null;
+        }
+
+        //function eraseCookie(name) {
+        //    createCookie(name, "", -1);
+        //}
+
+        var analytics = readCookie('analytics');
+
+        function acceptCookies() {
+            createCookie('analytics', 'true', 728);
+            $(window).trigger("cookies_accepted").off("scroll click", acceptCookies);
+        }
+
+        if (analytics == "true") {
+            $(window).trigger("cookies_accepted");
+        } else {
+            $('body').prepend($.cookiesParty.message);
+            $(window).on("scroll click", acceptCookies);
+        }
+    })(jQuery);
+}
